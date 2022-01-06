550a1c5be3d6ab16e68dd95d21a95920fe53488e
daviddlhz <vidda1959@gmail.com>
2022-01-06 00:53:08 -0500

    [FIXED] - Context of this in the method activeClickEventList found

--- src/Startup.ts
+++ src/Startup.ts
@@ -3,56 +3,58 @@ import { InputManipulation } from "./modules/InputManipulation";
 
 export class Startup {
 
+
   private calculator: Calcultator;
   private inputManipulation: InputManipulation;
+  
 
   constructor(_calculator: Calcultator, _inputManipulation: InputManipulation) {
-    
+
     this.calculator = _calculator;
     this.inputManipulation = _inputManipulation;
     
   }
 
   Init() {
-    
-    const inputs: NodeListOf<HTMLInputElement> = this.inputManipulation.getInputsElement("input[type=button]");
 
-    this.inputManipulation.activeClickEventList(inputs, this.Operations);
+    const func: Function = (inputValue: string, ev: Event) => this.Operations(inputValue, ev);
+
+    const inputs: HTMLInputElement[] = this.inputManipulation.getElements("input[type=button]");
+
+    this.inputManipulation.activeClickEventList(inputs, func);
 
   }
 
   Operations(inputValue: string, ev: Event) {
-    
+
     ev.preventDefault();
 
-    let numberOne: number =
-      this.inputManipulation.getInputValueNumber("numberOne");
+    let numberOne: number = this.inputManipulation.getValue("numberOne");
 
-    let numberTwo: number =
-      this.inputManipulation.getInputValueNumber("numberTwo");
+    let numberTwo: number = this.inputManipulation.getValue("numberTwo");
     
-      let resultOfOperation: number;
+    let resultOfOperation: number;
 
     switch (inputValue) {
       
       case "+":
         resultOfOperation = this.calculator.addition(numberOne, numberTwo);
-        this.inputManipulation.setInputValue("result", resultOfOperation)
+        this.inputManipulation.setValue("result", resultOfOperation)
         break;
 
       case "-":
         resultOfOperation = this.calculator.substraction(numberOne, numberTwo);
-        this.inputManipulation.setInputValue("result", resultOfOperation)
+        this.inputManipulation.setValue("result", resultOfOperation)
         break;
 
       case "*":
         resultOfOperation = this.calculator.multiplication(numberOne, numberTwo);
-        this.inputManipulation.setInputValue("result", resultOfOperation)
+        this.inputManipulation.setValue("result", resultOfOperation)
         break;
 
       case "/":
         resultOfOperation = this.calculator.division(numberOne, numberTwo);
-        this.inputManipulation.setInputValue("result", resultOfOperation);
+        this.inputManipulation.setValue("result", resultOfOperation);
         break;
 
       default:
